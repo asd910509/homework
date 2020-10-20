fd5a3a695a4cc32de7b95ab1e889920737a14bab
asd910509 <71751225+asd910509@users.noreply.github.com>
2020-10-20 11:53:45 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,24 @@
+basic.forever(function () {
+    for (let X = 0; X <= 4; X++) {
+        for (let Y = 0; Y <= 4; Y++) {
+            if (X == 0) {
+                led.plot(4 - X, 4 - Y)
+                basic.pause(200)
+            } else if (X == 1) {
+                led.plot(4 - X, Y)
+                basic.pause(200)
+            } else if (X == 2) {
+                led.plot(4 - X, 4 - Y)
+                basic.pause(200)
+            } else if (X == 3) {
+                led.plot(4 - X, Y)
+                basic.pause(200)
+            } else if (X == 4) {
+                led.plot(4 - X, 4 - Y)
+                basic.pause(200)
+            } else {
+            	
+            }
+        }
+    }
+})
